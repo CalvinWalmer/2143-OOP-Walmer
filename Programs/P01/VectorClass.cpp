90ea77fa88405a841ffbb8cce3950b99c0fbd1d3
CalvinWalmer <112140788+CalvinWalmer@users.noreply.github.com>
2022-09-13 13:16:43 -0500

    Add Program files

---
+++ Programs/P01/VectorClass.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Vector
+{
+    int* array;
+    int index;
+    int size;
+
+    Vector()
+    {
+        array = new int[10];
+        index = 0;
+        size = 10;
+    }
+
+    Vector(int *A, int size)
+    {
+
+    }
+
+    Vector(string fileName)
+    {
+
+    }
+
+    void Push()
+    {
+
+    }
+
+    int Pop()
+    {
+
+    }
+
+    void PushAt()
+    {
+
+    }
+
+    void Print()
+    {
+
+    }
+
+};
+
+
+int main()
+{
+    
+    return(0);
+}
