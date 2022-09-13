8b2dac6d3e2ddb27becca1f36e64358c434957a1
CalvinWalmer <112140788+CalvinWalmer@users.noreply.github.com>
2022-09-13 13:57:28 -0500

    Update VectorClass

--- Programs/P01/VectorClass.cpp
+++ Programs/P01/VectorClass.cpp
@@ -3,16 +3,39 @@
 
 using namespace std;
 
-class Vector
+struct Vector
 {
     int* array;
-    int index;
+    int vindex;
     int size;
+
+    bool isEmpty;
+
+    /*
+        Functions
+        PushFront(int val)
+        PushFront(Vector v)
+
+        PushRear(int val)
+        PushRear(Vector v)
+
+        PushAt(int location, int val)
+
+        InOrderPush(int val)
+
+        PopFront()
+
+        PopRear()
+
+        PopAt(int loc)
+
+        SearchPop(int val)
+    */
 
     Vector()
     {
         array = new int[10];
-        index = 0;
+        vindex = 0;
         size = 10;
     }
 
@@ -26,31 +49,69 @@ class Vector
 
     }
 
-    void Push()
+    ~Vector()
     {
+        delete[] array;
 
+    }
+
+
+    void Push(int val)
+    {
+        if (isFull()) 
+            Resize();
+
+        array[vindex] = val;
+        vindex++;
     }
 
     int Pop()
     {
-
+        return(0);
     }
 
-    void PushAt()
+    void PushAt(int index, int val)
     {
-
     }
 
     void Print()
     {
-
+        for(int i = 0; i < size; i++)
+        {
+            cout << array[i] << endl;
+        }
     }
+
+    bool isFull()
+    {
+        return vindex < size ? false : true;
+    }
+
+    void Resize()
+    {
+        int* temp;
+        size *= 2;
+        temp = new int[size];
+        for(int i; i < size; i++)
+        {
+            temp[i] = array[i];
+        }
+        array[0] = temp[0];
+        delete[] temp;
+    }
+
 
 };
 
 
 int main()
 {
-    
+    Vector V;
+    for(int i = 0; i < 15; i++)
+    {
+        V.Push(i);
+    }
+
+    V.Print();
     return(0);
 }
