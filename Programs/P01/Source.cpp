b1c3f08256b6349acae03e94af12e574ee7c71d2
CalvinWalmer <112140788+CalvinWalmer@users.noreply.github.com>
2022-09-19 01:07:22 -0500

    Update Source.cpp
    Successful build, still needs comments etc

--- Programs/P01/Source.cpp
+++ Programs/P01/Source.cpp
@@ -103,12 +103,11 @@ public:
         vindex++;
     }
 
-    void pushFront(Vector &v)
+    void pushFront(const Vector &v)
     {
         int newSize = vindex + v.vindex;
         int i, j;
-        int* temp = nullptr;
-        temp = new int[newSize];
+        int* temp = new int[newSize];
         for (i = 0; i < v.vindex; i++)
         {
             temp[i] = v.array[i];
@@ -135,7 +134,7 @@ public:
         vindex++;
     }
 
-    void pushRear(Vector &v)
+    void pushRear(const Vector &v)
     {
         int newSize = vindex + v.vindex;
         int i, j;
@@ -216,19 +215,19 @@ public:
     {
         ISEMPTY
 
-        int temp = -2;
+        int temp = -1;
         int i = 0;
 
         for(i = 0; i < vindex; i++)
         {
             if (val == array[i])
             {
-                temp = i;
+                temp = i + 1;
                 vindex--;
                 break;
             }
         }
-        if (temp)
+        if (temp != -1)
         {
             for(i = i; i < vindex; i++)
             {
@@ -236,7 +235,7 @@ public:
             }      
         }
 
-        return temp + 1;
+        return temp;
     }
 
     void print(int num = 0)
@@ -284,77 +283,6 @@ public:
 
 int main()
 {
-
-int x = 0;
-
-Vector v1;
-v1.pushFront(18);
-v1.pushFront(20);
-v1.pushFront(25);
-v1.pushRear(18);
-v1.pushRear(20);
-v1.pushRear(25);
-v1.print();
-// [25, 20, 18, 18, 20, 25]
-
-int A[] = {11,25,33,47,51};
-Vector v2(A,5);
-v2.print();
-// [11, 25, 33, 47, 51]
-
-v2.pushFront(9);
-//v2.inOrderPush(27);
-v2.pushRear(63);
-v2.print();
-// [9, 11, 25, 33, 47, 51, 63]
-
-v1.pushRear(v2);
-
-v1.print();
-// [25, 20, 18, 18, 20, 25, 9, 11, 25, 33, 47, 51, 63]
-
-x = v1.popFront();
-x = v1.popFront();
-x = v1.popFront();
-v1.print();
-// [18, 20, 25, 9, 11, 25, 27, 33, 47, 51, 63]
-cout<<x<<endl;
-// 18
-
-x = v1.popRear();
-x = v1.popRear();
-x = v1.popRear();
-v1.print();
-// [18, 20, 25, 9, 11, 25, 27, 33]
-cout<<x<<endl;
-// 47
-
-x = v2.popAt(3);
-v2.print();
-// [9, 11, 33, 47, 51, 63]
-cout<<x<<endl;
-// 25
-
-x = v2.find(51);
-cout<<x<<endl;
-// 5
-
-x = v2.find(99);
-cout<<x<<endl;
-// -1
-
-Vector v3(v1);
-v3.print();
-// [18, 20, 25, 9, 11, 25, 27, 33]
-
-v3.pushFront(v2);
-v3.print();
-//[9, 11, 25, 33, 47, 51, 63, 18, 20, 25, 9, 11, 25, 27, 33]
-
-Vector v4("input.dat");
-v4.pushRear(v3);
-v4.print();
-// [56, 61, 97, 66, 83, 25, 26, 11, 53, 49, 62, 18, 10, 18, 14, 3, 4, 23, 18, 24, 26, 27, 54, 14, 12, 45, 65, 98, 56, 97, 15, 84, 98, 9, 11, 25, 33, 47, 51, 63, 18, 20, 25, 9, 11, 25, 27, 33]
 
     return(0);
 }
